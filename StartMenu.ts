2c8e078fb3e4ddb64d75d2c77f26e4861d93a686
Athallah 28000 <dqdalimunthe.13@gmail.com>
2025-10-17 22:19:30 +0700

    Update StartMenu.ts

--- StartMenu.ts
+++ StartMenu.ts
@@ -7,13 +7,39 @@ export class StartMenu extends Component {
     @property(Button)
     playButton: Button = null!;
 
+    @property(Button)
+    hardcoreButton: Button = null!;   // <-- new Hardcore button
+
+    @property(Button)
+    oppButton: Button = null!; 
+
+    
+
     start() {
         // Connect Play button
         this.playButton.node.on(Button.EventType.CLICK, this.onPlay, this);
+
+        // Connect Hardcore button
+        this.hardcoreButton.node.on(Button.EventType.CLICK, this.onHardcore, this);
+
+        
+        this.oppButton.node.on(Button.EventType.CLICK, this.onOpp, this);
+
+        
     }
 
     onPlay() {
         // Load the main game scene
         director.loadScene("GAME");
+    }
+
+    onHardcore() {
+        // Load the Hardcore scene
+        director.loadScene("HARDCORE");
+    }
+
+    onOpp() {
+
+        director.loadScene("OPP");
     }
 }
